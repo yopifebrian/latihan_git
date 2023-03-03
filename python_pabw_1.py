e3790ac0a77059311082bf2039134f8d37606d85
yopifebrian <91509334+yopifebrian@users.noreply.github.com>
2023-03-03 11:17:08 +0700

    Cek fungsi

--- python_pabw_1.py
+++ python_pabw_1.py
@@ -1,3 +1,8 @@
-for i in range(1,101):
-    if i%2!=0:
-        print(i)
+def looping(x):
+    if x%2!=0:
+        hasil = "Ganjil"
+    else:
+        hasil = "Genap"
+    return hasil
+
+print(looping(12))
