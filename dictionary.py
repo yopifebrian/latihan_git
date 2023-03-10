5b1880139f4d71177d0aee023b719eb1a31442aa
yopifebrian <91509334+yopifebrian@users.noreply.github.com>
2023-03-10 10:40:44 +0700

    Commit v1.2 - Pencarian berdasarkan nama
    Dalam program yang sama, buatlah program untuk melakukan pencarian ke dalam dictionary yang sudah ditulis. Lakukan pencarian berdasarkan input yang diterima pada nomor (2) Lakukan pencarian berdasarkan atribut nama. Jika nama yang dicari ada dalam dictionary, tampilkan data lengkapnya dari nama hingga tanggal lahir. Jika tidak ditemukan nama yang dicari, tampilkan “data tidak ada” (commit v1.2)

--- dictionary.py
+++ dictionary.py
@@ -3,11 +3,30 @@ user = [{"nama": 'arsene lupin', "nik": 1234, "jenis_kelamin": "male", "tanggal_
 {"nama": "irene adler", "nik":6789, "jenis_kelamin": "female", "tanggal_lahir":"1884-10-07"},]
 
 print(user)
-
-nama = input("Apa nama anda? ")
-nik = int(input("Apa nik anda? "))
-jenis_kelamin = input("Apa jenis kelamin anda? ")
-tanggal_lahir = input("Apa tanggal lahir anda? ")
+inp=999
+while inp!=0:
+  print("""
+  1. isi Data
+  2. Cari berdasar nama
+  0. keluar""")
+  inp = int(input("Pilih menu: "))
+  if inp == 1:
+    nama = input("Apa nama anda? ")
+    nik = int(input("Apa nik anda? "))
+    jenis_kelamin = input("Apa jenis kelamin anda? ")
+    tanggal_lahir = input("Apa tanggal lahir anda? ")
+    break
+  elif inp == 2:
+    cari= input("Masukkan nama yang dicari: ")
+    for y in user:
+      for key, value in y.items():
+        if value == cari:
+          print("user",cari,"telah ditemukan")
+          break
+        else:
+          hasil = 0
+        if hasil == 0:
+          break
 
 user_dict= {"nama": nama, "nik": nik, "jenis_kelamin":jenis_kelamin, "tanggal_lahir":tanggal_lahir}
 user.append(user_dict)
