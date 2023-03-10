ada61baa61aefea83c0af40f30dbb6c28d9c3922
yopifebrian <91509334+yopifebrian@users.noreply.github.com>
2023-03-10 10:16:21 +0700

    Commit v1.0 - Buat Dictionary
    Buatlah program python dengan struktur dictionary seperti di atas (commit v1.0)

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+user = [{"nama": 'arsene lupin', "nik": 1234, "jenis_kelamin": "male", "tanggal_lahir":"1902-03-23"},
+{"nama": "sherlock holmes", "nik": 4321, "jenis_kelamin": "male", "tanggal_lahir":"1876-08-16"},
+{"nama": "irene adler", "nik":6789, "jenis_kelamin": "female", "tanggal_lahir":"1884-10-07"},]
+print(user)
